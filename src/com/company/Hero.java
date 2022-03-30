fea184451959864a7517506a1fc3b3c9ad13636e
Marsel151205 <mambetalievmarsel2289@gmail.com>
2022-03-30 14:46:27 +0600

    Основная домашка и доп дз

--- src/com/company/Hero.java
+++ src/com/company/Hero.java
@@ -5,15 +5,27 @@ public class Hero {
     private int heroDamage;
     private String superPower;
 
-    public void Hero(int heroHealth, int heroDamage, String superPower){
+    public Hero(int heroHealth, int heroDamage, String superPower){
         this.heroHealth = heroHealth;
         this.heroDamage = heroDamage;
         this.superPower = superPower;
     }
 
-    public void Hero(int heroHealth, int heroDamage){
+    public Hero(int heroHealth, int heroDamage){
         this.heroHealth = heroHealth;
         this.heroDamage = heroDamage;
+    }
+
+    public void setHeroHealth(int heroHealth) {
+        this.heroHealth = heroHealth;
+    }
+
+    public void setHeroDamage(int heroDamage) {
+        this.heroDamage = heroDamage;
+    }
+
+    public void setSuperPower(String superPower) {
+        this.superPower = superPower;
     }
 
     public int getHeroHealth() {
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,20 @@ public class Main {
         boss.setBossDamage(50);
         boss.setDefenceType("Magical");
         System.out.println("Здоровье Босса: " + boss.getBossHealth() + "   Урон Босса: " + boss.getBossDamage()
-        + "  Тип защиты: " + boss.getDefenceType());
+                + "  Тип защиты: " + boss.getDefenceType());
+       // System.out.println(createHeroes());
+        for (Hero hero: createHeroes()) {
+            System.out.println(hero.getHeroHealth() + " [" + hero.getHeroDamage() +"] " + hero.getSuperPower());
+        }
+    }
+
+    public static Hero[] createHeroes() {
+        Hero warrior = new Hero(250,30,"Battle Cry");
+
+        Hero ninja = new Hero(180, 20, "Destruction");
+
+        Hero mage = new Hero(220, 35, "Icing");
+
+        return new Hero[]{warrior, ninja, mage};
     }
 }
